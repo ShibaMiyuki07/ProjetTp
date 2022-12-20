554bd1877f7908adc774bd5a720885ed133a24c5
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2022-12-20 12:48:08 +0300

    handle error

--- ionic/src/pages/Home.tsx
+++ ionic/src/pages/Home.tsx
@@ -1,8 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonLabel, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [data,setData] = useState([]);
+  const [erreur,setErreur] = useState("");
+  const [isOpen,setIsOpen] = useState(false);
+  useEffect(() =>
+  {
+    axios.get("autre/test").then(data =>{
+      console.log(data.data);
+      console.log(data.request['status']);
+      if(data.request['status'] !== 200)
+      {
+        setIsOpen(true);
+        setErreur("Veuillez vous connecter");
+      }
+      else
+      {
+        setData(data.data);
+      }
+    }).catch((err) =>
+    {
+      setIsOpen(true);
+      setErreur("Veuillez vous connecter");
+    })
+  })
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +41,10 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonModal isOpen={isOpen}>
+          <IonLabel>{erreur}</IonLabel>
+          <IonButton>OK</IonButton>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
