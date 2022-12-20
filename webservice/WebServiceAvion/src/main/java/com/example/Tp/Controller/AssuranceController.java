3e040320dda6871a5afa8f867c915208f7ec0df8
Mianta23 <rajerymianta@gmail.com>
2022-12-20 12:39:43 +0100

    assurance

--- webservice/WebServiceAvion/src/main/java/com/example/Tp/Controller/AssuranceController.java
+++ webservice/WebServiceAvion/src/main/java/com/example/Tp/Controller/AssuranceController.java
@@ -5,12 +5,12 @@ package com.example.Tp.Comtroller;
 public class AssuranceController {
     
     @GetMapping("/assurance/{mois}")
-    public ArrayList<Avion> getEtat(@PathVariable("mois") int mois) throws SQLException {
+    public ArrayList<Assurance> getEtat(@PathVariable("mois") int mois) throws SQLException {
         AssuranceDao v = new AssuranceDao();
         return v.findBy(mois);
     }
-    @GetMapping("/assurance/")
-    public ArrayList<Avion> getAll() throws SQLException {
+    @GetMapping("/assurance")
+    public ArrayList<Assurance> getAll() throws SQLException {
         AssuranceDao v = new AssuranceDao();
         return v.findAll();
     }
