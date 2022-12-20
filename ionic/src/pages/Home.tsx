5b075db5eea1ba8b0c5ca5a3cb73c9bc3970768d
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2022-12-20 12:01:18 +0300

    build android

--- ionic/src/pages/Home.tsx
+++ ionic/src/pages/Home.tsx
@@ -1,8 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonLabel, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { render } from '@testing-library/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+const getData = () =>{
+  return axios.get("http:localhost:8080/").then((data : any) =>{
+    return data.data;
+  }).catch((err) =>
+  {
+     return "Connexion echoue";
+  })
+}
+export const http = axios.create({
+  baseURL : "http://localhost:8080/",
+  timeout : 1000
+})
 const Home: React.FC = () => {
+  const [isOpen,setIsOpen] = useState(false);
+  const [error,setError] = useState("");
+  const [item,setItem] = useState([]);
+  useEffect(() =>
+  {
+    http.interceptors.request.use()
+    getData().then((data) =>{
+      if(data !== error)
+      {
+
+      }
+      setItem(data)
+      
+    }
+  })
+>>>>>>> Stashed changes
   return (
     <IonPage>
       <IonHeader>
@@ -15,6 +46,12 @@ const Home: React.FC = () => {
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
+<<<<<<< Updated upstream
+=======
+          <IonModal isOpen={isOpen}>
+            <IonLabel>{error}</IonLabel>
+          </IonModal>
+>>>>>>> Stashed changes
         </IonHeader>
         <ExploreContainer />
       </IonContent>
